697112c729ad00ac7c0b1c20a02358ca8889a456
Kaspar Vollenweider <vok@panter.ch>
2023-03-12 00:04:30 +0100

    fix some dev server stuff

--- apps/speak-server/src/main.ts
+++ apps/speak-server/src/main.ts
@@ -5,19 +5,39 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { resolve } from 'path';
+import { existsSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
 
 import { AppModule } from './app/app.module';
 
+const pidFilePath = resolve('/tmp/speak-server.pid');
+
+
+const killIfOpen = async () => {
+  if (!existsSync(pidFilePath)) {
+    return;
+  }
+  const pidBuffer = await readFile(pidFilePath);
+  const pid = parseInt(pidBuffer.toString(), 10);
+  console.log(`Killing existing process ${pid}`);
+  try {
+    process.kill(pid);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function bootstrap() {
+  await killIfOpen();
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
-
+  await writeFile(pidFilePath, String(process.pid));
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}\n
-    PID: ${process.pid}`
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix} with PID ${process.pid}`
   );
 }
 
